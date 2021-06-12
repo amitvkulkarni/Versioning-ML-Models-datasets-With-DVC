3e52bf758cee526baa9d6948154af72d1960c512
Amit Kulkarni <amit.vishwanath@gmail.com>
2021-06-12 23:41:47 +0530

    Experiment with plots

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -4,12 +4,15 @@
 # train algo
 # save the metrices, params
 import os
+import math
 import warnings
 import sys
 import pandas as pd
 import numpy as np
+import sklearn.metrics as metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, plot_roc_curve,accuracy_score
 from sklearn.metrics import average_precision_score
 from get_data import read_params
@@ -24,7 +27,6 @@ def train_and_evaluate(config_path):
     train_data_path = config["split_data"]["train_path"]
     random_state = config["base"]["random_state"]
     model_dir = config["model_dir"]
-
     
     target = [config["base"]["target_col"]]
 
@@ -39,7 +41,7 @@ def train_and_evaluate(config_path):
     
 
     # Build logistic regression model
-    model = LogisticRegression(solver='lbfgs', random_state=0).fit(train_x, train_y)
+    model = LogisticRegression(solver='liblinear', random_state=0).fit(train_x, train_y)
     
     # Report training set score
     train_score = model.score(train_x, train_y) * 100
@@ -49,6 +51,49 @@ def train_and_evaluate(config_path):
     print(test_score)
 
     predicted_val = model.predict(test_x)
+
+    #----------------------------------------------------------------------------------------------------------------------------
+       
+    precision, recall, prc_thresholds = metrics.precision_recall_curve(test_y, predicted_val)
+    fpr, tpr, roc_thresholds = metrics.roc_curve(test_y, predicted_val)
+
+    avg_prec = metrics.average_precision_score(test_y, predicted_val)
+    roc_auc = metrics.roc_auc_score(test_y, predicted_val)
+
+    scores_file = config["reports"]["scores"]
+    prc_file = config["reports"]["prc"]
+    roc_file = config["reports"]["roc"]
+
+    # with open(scores_file, "w") as fd:
+    #     json.dump({"avg_prec": avg_prec, "roc_auc": roc_auc}, fd, indent=4)
+
+    
+    nth_point = math.ceil(len(prc_thresholds) / 1000)
+    prc_points = list(zip(precision, recall, prc_thresholds))[::nth_point]    
+    
+    
+    with open(prc_file, "w") as fd:
+        prcs = {
+                "prc": [
+                    {"precision": p, "recall": r, "threshold": t}
+                    for p, r, t in prc_points
+                ]
+            }
+        json.dump(str(prcs), fd, indent=4)
+        
+
+    with open(roc_file, "w") as fd:
+        rocs = {
+                "roc": [
+                    {"fpr": fp, "tpr": tp, "threshold": t}
+                    for fp, tp, t in zip(fpr, tpr, roc_thresholds)
+                ]
+            }
+        json.dump(str(rocs), fd, indent=4)
+        
+
+    #----------------------------------------------------------------------------------------------------------------------------
+
     # Print classification report
     print(classification_report(test_y, predicted_val))
 
@@ -59,27 +104,46 @@ def train_and_evaluate(config_path):
     roc_auc = roc_auc_score(test_y, model.predict_proba(test_x)[:, 1])
     print('ROC_AUC:{0:0.2f}'.format(roc_auc))
 
-    accuracy = accuracy_score(test_y, predicted_val)
-    print('Model Accuracy:{0:0.2f}'.format(accuracy))
+    Logistic_Accuracy = accuracy_score(test_y, predicted_val)
+    print('Logistic Regression Model Accuracy:{0:0.2f}'.format(Logistic_Accuracy))
 
     # Average precision score
     average_precision = average_precision_score(test_y, predicted_val)
     print('Average precision-recall score: {0:0.2f}'.format(average_precision))
 
-    scores_file = config["reports"]["scores"]
+    #---------------- Random Forest -----------------------------
+
+    # model_rf = RandomForestClassifier(n_estimators = 50)  
+    
+    # model_rf.fit(train_x, train_y)
+    
+    # # performing predictions on the test dataset
+    # pred_rf = model_rf.predict(test_x)
+    
+    # # metrics are used to find accuracy or error
+        
+    
+    # # using metrics module for accuracy calculation
+    # RF_Accuracy = metrics.accuracy_score(test_y, pred_rf)
+    # print("Random Forest Accuracy: ", RF_Accuracy)
+
+    #-------------------------------------------------------------------
+
+    # scores_file = config["reports"]["scores"]
     
     with open(scores_file, "w") as f:
         scores = {
             "train_score": train_score,
             "test_score": test_score,
             "roc_auc": roc_auc,
+            "Precision": precision,
+            "Recall": recall,
             "Average precision": average_precision,
-            "Model Accuracy": accuracy
-
-                                  
+            "Logistic Accuracy": Logistic_Accuracy
+            # "Random Forest Accuracy": RF_Accuracy                                 
             
         }
-        json.dump(scores, f, indent=4)
+        json.dump(str(scores), f, indent=4)
 
 
     os.makedirs(model_dir, exist_ok=True)
