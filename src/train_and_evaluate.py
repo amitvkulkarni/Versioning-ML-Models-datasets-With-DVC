9a8057e45cc9a18c1c704b1ac6c1f059a25e011a
Amit Kulkarni <amit.vishwanath@gmail.com>
2021-06-23 18:28:09 +0530

    code clean up

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -154,7 +154,6 @@ def train_and_evaluate(config_path):
     print("600 KB dataset with solver as saga")
 
 
-
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
